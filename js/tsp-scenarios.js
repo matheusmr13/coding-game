17af87f5bb75bad6a6d08cb5189d0caf32761867
Tomas <tomas.queiroga@dextra-sw.com>
2017-07-29 10:15:33 -0300

    tsp

--- js/tsp-scenarios.js
+++ js/tsp-scenarios.js
@@ -19,22 +19,22 @@
 		},
 		3 : (w, h) => {
 			return {
-				player :  { x : w / 2, y : 2 * h / 3},
-				targets : [{x : w / 5, y : h / 4}, {x : 2 * w / 5, y : h / 4}, {x : 3 * w / 5, y : h / 4}, {x : 4 * w / 5, y : h / 4}],
+				player :  { x : w / 2, y : h / 2},
+				targets : [{x : w / 5, y : h / 4}, {x : 2 * w / 5, y : h / 4}, {x : 3 * w / 5, y : h / 4}, {x : 4 * w / 5, y : h / 4},
+						   {x : w / 5, y : 3 * h / 4}, {x : 2 * w / 5, y : 3 * h / 4},
+						   {x : w / 5, y : h / 2}, {x : 4 * w / 5, y : h / 2}],
 				power : {x : 3 * w / 4, y : 3 * h / 4},
 				myScore : 3
 			}
 		},
 		4 : (w, h) => {
 			return {
-				player :  { x : w / 2, y : h / 2},
-				targets : [{x : w / 5, y : h / 4}, {x : 2 * w / 5, y : h / 4}, {x : 3 * w / 5, y : h / 4}, {x : 4 * w / 5, y : h / 4},
-						   {x : w / 5, y : 3 * h / 4}, {x : 2 * w / 5, y : 3 * h / 4}, {x : 3 * w / 5, y : 3 * h / 4}, {x : 4 * w / 5, y : 3 * h / 4},
-						   {x : w / 5, y : h / 2}, {x : 4 * w / 5, y : h / 2}],
-				power : {x : 3 * w / 4, y : 3 * h / 4},
+				player :  { x : w / 2, y : 2 * h / 3},
+				targets : [{x : w / 5, y : h / 4}, {x : 2 * w / 5, y : h / 4}, {x : 3 * w / 5, y : h / 4}, {x : 4 * w / 5, y : h / 4}],
+				power : {x : w / 2, y : h / 5},
 				myScore : 3
 			}
-		}
+		},
 	}
 
 	this.scenarios = scenarios
